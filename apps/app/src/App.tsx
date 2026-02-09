b48a212f9a7556ba54f7f604ddd2261ced112d41
5ock <petestein88@gmail.com>
2026-02-10 09:32:28 +1100

    feat: copy App.tsx from iot-frontend

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -1,15 +1,82 @@
-// This file will be replaced with the full App.tsx from iot-frontend
-// Placeholder for now
-import { Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
+import NotFound from './common/NotFound'
+import Layout from './components/Layout'
+import { ENV_CONFIG } from './config/env.config'
+import Challenges from './pages/Challenges'
+import Data from './pages/Data'
+import Friends from './pages/Friends'
+import Home from './pages/Home'
+import SignIn from './pages/Login'
+import SignUp from './pages/Register'
+import Settings from './pages/Setting'
+import Faqs from './pages/faqs/Faqs'
+import { utils } from './utils'
 
 function App() {
-  return (
-    <div className="min-h-screen bg-sacred-black text-sacred-white">
-      <Routes>
-        <Route path="/" element={<div className="p-8">Sacred Dashboard Coming Soon...</div>} />
-      </Routes>
-    </div>
-  )
+    const { pathname } = useLocation()
+    const navigate = useNavigate()
+    const [routeType, setRouteType] = useState(utils.helpers.getRouteType(pathname))
+
+    useEffect(() => {
+        const urlPrefix = ENV_CONFIG.URL_PREFIX
+
+        // Ensure we always have a valid path
+        const updatedPathname = utils.string.removeTrailingSlash(pathname)
+
+        if (urlPrefix && pathname === '/') {
+            navigate(utils.helpers.getRoute('/'))
+            return
+        }
+
+        setRouteType(utils.helpers.getRouteType(updatedPathname))
+        window.scrollTo(0, 0)
+    }, [pathname, navigate])
+
+    if (routeType.protected) {
+        return (
+            <Routes>
+                <Route path={utils.helpers.getRoute('/')} element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path={utils.helpers.getRoute('/data')} element={<Data />} />
+                    <Route path={utils.helpers.getRoute('/friends')} element={<Friends />} />
+
+                    <Route path={utils.helpers.getRoute('/challenges')} element={<Challenges />} />
+                    <Route path={utils.helpers.getRoute('/settings')} element={<Settings />} />
+                    <Route path={utils.helpers.getRoute('/faqs')} element={<Faqs />} />
+                </Route>
+            </Routes>
+        )
+    }
+
+    return (
+        <Routes>
+            <Route
+                path={utils.helpers.getRoute('/auth/signin')}
+                element={
+                    <>
+                        <SignIn />
+                    </>
+                }
+            />
+            <Route
+                path={utils.helpers.getRoute('/auth/signup')}
+                element={
+                    <>
+                        <SignUp />
+                    </>
+                }
+            />
+            <Route
+                path='*'
+                element={
+                    <>
+                        <NotFound />
+                    </>
+                }
+            />
+        </Routes>
+    )
 }
 
 export default App
